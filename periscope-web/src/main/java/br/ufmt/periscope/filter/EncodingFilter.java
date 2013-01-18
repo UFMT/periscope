0652885da8c30f04df3e03451806e6dc365f66cb
Andre Peripolli <andre@MacBook-Air-de-Andre.local>
2013-01-18 12:13:10 -0300

    Adicao de algumas messages e algumas pastas para estrutura de paginas

---
+++ periscope-web/src/main/java/br/ufmt/periscope/filter/EncodingFilter.java
@@ -0,0 +1,34 @@
+package br.ufmt.periscope.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns={"*"})
+public class EncodingFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {		
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+	
+	@Override
+	public void destroy() {		
+		
+	}	
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+
+		
+	}
+
+}
