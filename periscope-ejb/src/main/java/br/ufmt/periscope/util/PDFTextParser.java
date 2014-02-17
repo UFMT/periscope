906477bdb54a19195e6253d773911f43405909e9
Cristhian Ferreira <cmf_352@hotmail.com>
2014-02-17 10:52:20 -0400

    Criada a classe responsável por converter pdf em txt e adicionada as dependências para as bibliotecas necessárias na classe.

---
+++ periscope-ejb/src/main/java/br/ufmt/periscope/util/PDFTextParser.java
@@ -0,0 +1,57 @@
+package br.ufmt.periscope.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+/**
+ *
+ * @author cristhian
+ */
+public class PDFTextParser {
+    PDFParser parser;
+    String parsedText;
+    PDFTextStripper pdfStripper;
+    PDDocument pdDoc;
+    COSDocument cosDoc;
+    PDDocumentInformation pdInformation;
+    
+    String pdfToText(String fileName) throws IOException {
+        File file = new File(fileName);
+        try {
+            parser = new PDFParser(new FileInputStream(file));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PDFTextParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        parser.parse();
+        cosDoc = parser.getDocument();
+        pdfStripper = new PDFTextStripper();
+        pdDoc = new PDDocument(cosDoc);
+        parsedText = pdfStripper.getText(pdDoc);
+        
+        return parsedText;
+    }
+    
+    void writeTextToFile(String pdfText, String fileName) {
+        
+        
+        try {
+            PrintWriter pw = new PrintWriter(fileName);
+            pw.print(pdfText);
+            pw.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PDFTextParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
