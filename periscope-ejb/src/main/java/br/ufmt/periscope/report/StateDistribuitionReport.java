d9b751908712b1329e436aaabd437fc8d047e96e
endgame01 <phellippe.end@gmail.com>
2014-05-24 16:07:02 -0400

    Grafico distribuição por estado adicionado

---
+++ periscope-ejb/src/main/java/br/ufmt/periscope/report/StateDistribuitionReport.java
@@ -0,0 +1,34 @@
+package br.ufmt.periscope.report;
+
+import br.ufmt.periscope.model.Project;
+import br.ufmt.periscope.repository.StateDistribuitionRepository;
+import java.util.Collections;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.model.chart.ChartSeries;
+
+@Named
+public class StateDistribuitionReport {
+
+	private @Inject	StateDistribuitionRepository repo;
+        
+
+	public ChartSeries StateDistribuitionSeries(Project currentProject) {
+		ChartSeries series = new ChartSeries("Distribuição por Estados");
+                System.out.println("entrou report");
+		List<Pair> i = repo.getStateDistribuitions(currentProject);
+                System.out.println("saiu report");
+                Collections.reverse(i);
+		
+		for (Pair pair : i) {
+                        String state = (String)pair.getKey();
+			//System.out.println(aux.get("applicationPerYear"));
+			Integer count = (Integer)pair.getValue();
+			series.set(state, count);
+		}		
+
+		return series;
+	}
+
+}
