9d22043fc719bfd9c811a0363d43fb02f4d0ba25
thesivis <raphael@ic.ufmt.br>
2014-05-29 11:21:40 -0400

    Arrumando o multiple da importação

--- periscope-web/src/main/java/br/ufmt/periscope/controller/ImportPatentController.java
+++ periscope-web/src/main/java/br/ufmt/periscope/controller/ImportPatentController.java
@@ -21,6 +21,10 @@ import br.ufmt.periscope.qualifier.CurrentProject;
 import br.ufmt.periscope.repository.PatentRepository;
 
 import com.github.jmkgreen.morphia.Datastore;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @ManagedBean
 @ViewScoped
 public class ImportPatentController implements Serializable {
@@ -37,7 +41,7 @@ public class ImportPatentController implements Serializable {
     PatentImporterFactory importerFactory;
     private String fileOrigin;
     private String[] origins = null;
-    private UploadedFile fileUploaded = null;
+    private List<UploadedFile> uploadAttachment = new ArrayList<UploadedFile>();
 
     @PostConstruct
     public void init() {
@@ -45,23 +49,31 @@ public class ImportPatentController implements Serializable {
     }
 
     public void importPatents() {
-        if (fileUploaded == null) {
+        if (uploadAttachment == null || uploadAttachment.isEmpty()) {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Nenhum arquivo foi enviado.");
             FacesContext.getCurrentInstance().addMessage(null, msg);
         } else {
             try {
-                InputStream is = fileUploaded.getInputstream();
-                PatentImporter importer = importerFactory.getImporter(fileOrigin);
-                if (importer.initWithStream(is)) {
+                boolean imported = true;
+                List<String> errors = new ArrayList<String>();
+                for (UploadedFile file : uploadAttachment) {
 
-                    patentRepository.savePatentToDatabase(importer, currentProject);
+                    InputStream is = file.getInputstream();
+                    PatentImporter importer = importerFactory.getImporter(fileOrigin);
+                    if (importer.initWithStream(is)) {
+                        patentRepository.savePatentToDatabase(importer, currentProject);
 
+//                        System.out.println(currentProject.getPatents().size());
+                    } else {
+                        imported = false;
+                        errors.add(file.getFileName());
+                    }
+                }
+                if (imported) {
                     FacesMessage msg = new FacesMessage("Sucesso", "Arquivo importado com sucesso.");
                     FacesContext.getCurrentInstance().addMessage(null, msg);
-
-                    System.out.println(currentProject.getPatents().size());
                 } else {
-                    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Ocorreu um erro com o arquivo que foi enviado.");
+                    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Ocorreu um erro com o(s) arquivo(s) enviado(s) " + Arrays.toString(errors.toArray()) + ".");
                     FacesContext.getCurrentInstance().addMessage(null, msg);
                 }
 
@@ -75,13 +87,9 @@ public class ImportPatentController implements Serializable {
     }
 
     public void handleFileUpload(FileUploadEvent event) {
-
-        fileUploaded = event.getFile();
+        uploadAttachment.add(event.getFile());
         FacesMessage msg = new FacesMessage("Sucesso", event.getFile().getFileName() + " foi enviado.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-  public UploadedFile getFileUploaded() {
-        return fileUploaded;
     }
 
     public String getFileOrigin() {
@@ -95,4 +103,13 @@ public class ImportPatentController implements Serializable {
     public String[] getOrigins() {
         return origins;
     }
+
+    public List<UploadedFile> getUploadAttachment() {
+        return uploadAttachment;
+    }
+
+    public void setUploadAttachment(List<UploadedFile> uploadAttachment) {
+        this.uploadAttachment = uploadAttachment;
+    }
+
 }
