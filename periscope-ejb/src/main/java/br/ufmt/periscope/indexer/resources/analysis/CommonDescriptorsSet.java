54615551a7958f0def5c4b9a7256cf1f50f519c3
mattyws <mattyws@gmail.com>
2014-08-01 23:41:16 -0400

    Bug no ultimo commit

---
+++ periscope-ejb/src/main/java/br/ufmt/periscope/indexer/resources/analysis/CommonDescriptorsSet.java
@@ -0,0 +1,100 @@
+package br.ufmt.periscope.indexer.resources.analysis;
+
+import com.github.jmkgreen.morphia.Datastore;
+import com.mongodb.Mongo;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+
+/**
+ * This class will contains the commons company descriptors Now, it's just use a
+ * ArrayList, but this may change in the future
+ *
+ * @author mattyws
+ */
+@Named
+public class CommonDescriptorsSet {
+
+    private @Inject IndexReader inReader;
+    private @Inject Datastore ds;
+
+    private List<String> commonDescriptors = new ArrayList<String>();
+    private BufferedReader reader;
+    private IndexSearcher searcher;
+
+    /**
+     * Constructor for Lucene based CommonDescriptorsSet
+     */
+    public CommonDescriptorsSet() {
+//        searcher = new IndexSearcher(inReader);
+    }
+
+    /**
+     * The constructor
+     *
+     * @param caminho path for the file containing the commons descriptors
+     */
+//    public CommonDescriptorsSet(String caminho) {
+//        try {
+//            File file = new File(caminho);
+//            reader = new BufferedReader(new FileReader(caminho));
+//            this.readFile();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        
+//    }
+    /**
+     * Read the file passed by parameter in the constructor
+     *
+     * @throws IOException
+     */
+//    private void readFile() throws IOException {
+//        String linha;
+//        while (reader.ready()) {
+//            linha = reader.readLine();
+//            commonDescriptors.add(linha);
+//        }
+//        reader.close();
+//    }
+    /**
+     * See if a string exists in the list
+     *
+     * @param descriptor the text that will be searched
+     * @return true if exists in list, false otherwise
+     */
+//    public boolean contains(String descriptor) {
+//        return commonDescriptors.contains(descriptor);
+//    }
+    public boolean contains(String descriptor) {
+//        Query query = new TermQuery(new Term("id", descriptor));
+
+//        try {
+//            ScoreDoc[] hits = searcher.search(query, 10).scoreDocs;
+//            for (int i = 0; i < hits.length; ++i) {
+//                int docId = hits[i].doc;
+//                Document d = searcher.doc(docId);
+//                System.out.println(descriptor + " retornou : " + d.get("id"));
+//            }
+//            if(hits.length > 0) {
+//                return true;
+//            }
+//        } catch (IOException ex) { 
+//            Logger.getLogger(CommonDescriptorsSet.class.getName()).log(Level.SEVERE, null, ex);
+//        }        
+        CommonDescriptor descriptorSet = ds.find(CommonDescriptor.class).field("_id").equal(descriptor).get();
+
+        
+        if (descriptorSet != null) {            
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ periscope-ejb/src/main/java/br/ufmt/periscope/indexer/resources/analysis/CommonDescriptorsTokenFilter.java
@@ -0,0 +1,140 @@
+package br.ufmt.periscope.indexer.resources.analysis;
+
+import com.github.jmkgreen.morphia.Datastore;
+import com.mongodb.Mongo;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+
+/**
+ * The token filter that will remove the Common company descriptors
+ *
+ * @author mattyws
+ */
+public class CommonDescriptorsTokenFilter extends TokenFilter {
+
+    // The attribute to manupulate the token as a string
+    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    // The attribute to control the positions of the tokens
+    private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+    // This is the set for the common descriptors, this is the file for the descriptors
+    private CommonDescriptorsSet descriptorsSet;
+    // This will contain the tokens, because is more easy to control in a list
+    public List<String> tokens = new ArrayList<String>();
+    // Bolleans to help the operations
+    public boolean consumed = false, removed = false, constructed = false,
+            removeIt;
+    // Auxiliar variables
+    public int pos, start;
+    public String text;
+
+    /**
+     * The default constructor for TokenFilter
+     *
+     * @param input the input TokenStream
+     */
+    public CommonDescriptorsTokenFilter(TokenStream input, CommonDescriptorsSet descriptorsSet) {
+        super(input);
+        this.descriptorsSet = descriptorsSet;
+    }
+
+    @Override
+    public boolean incrementToken() throws IOException {
+        // If we not consumed all tokens, then cosume it!
+        if (!consumed) {
+            consumed = true;
+            while (input.incrementToken()) {
+                if(!termAtt.toString().replaceAll("[^A-Za-z0-9_]", "").isEmpty())
+                    tokens.add(termAtt.toString().replaceAll("[^A-Za-z0-9_]", ""));
+            }
+            // The start position
+            pos = 0;
+            return true;
+        } else {
+            // If we not remove the common descriptors, and the size of the
+            // name it's greater than 2
+            if (!removed && tokens.size() > 2) {
+                // Remove the common descriptors
+                while (pos < tokens.size()) {
+                    if (descriptorsSet.contains(tokens.get(pos).trim())) {                        
+                        // In there we may remove it
+                        removeIt = true;
+                        int j = pos + 1;
+                        while (j < tokens.size() && removeIt && pos != 0) {
+                            // But if have some word after this that is not an
+                            // common descriptor, we will not remove it
+                            if (tokens.get(j).length() > 1 && !descriptorsSet.contains(tokens.get(j).trim())) {
+                                removeIt = false;
+                            }
+                            j += 1;
+                        }
+                        // If we need to remove, so, remove
+                        if (removeIt) {                            
+                            tokens.remove(pos);
+                            pos -= 1;
+                        }
+                    }
+                    pos += 1;
+                }
+                removed = true;
+                return true;
+            }
+        }
+        // If we already pass through the remove operation, so let's construct
+        // the TokenStream
+        if (removed || tokens.size() <= 2) {
+            // if we not contruct the text, so construct it, this is better to
+            // create the TokenStream
+            if (!constructed) {
+                text = "";
+                for (String token : tokens) {
+                    text = text + token + " ";
+                }
+                constructed = true;
+                text = text.trim();
+                // Flush the input TokenStream to create the TokenStream without
+                // the common descriptors
+                termAtt.setEmpty();
+                start = 0;
+            }
+            // Creates the new TokenStream
+            if (start < text.length() && !text.isEmpty()) {
+                int i;
+                for (i = start; i < text.length(); i++) {
+                    // If it's a withespace character, create the token
+                    if (Character.isWhitespace(text.charAt(i))) {
+                        offsetAtt.setOffset(start, i);
+                        termAtt.copyBuffer(text.toCharArray(), start, i - start);
+                        start = i + 1;
+                        return true;
+                    }
+                }
+                // If we research to the final of the string, create the last 
+                // token
+                if (i == text.length()) {
+                    offsetAtt.setOffset(start, i);
+                    termAtt.copyBuffer(text.toCharArray(), start, i - start);
+                    start = i + 1;
+                    return true;
+                }
+            }
+        }
+        // Flush the results because the reuse strategy of the Lucene
+        this.flush();
+        return false;
+    }
+
+    private void flush() {
+        pos = 0;
+        tokens = new ArrayList<String>();
+        consumed = false;
+        removed = false;
+        constructed = false;
+    }
+
+}
